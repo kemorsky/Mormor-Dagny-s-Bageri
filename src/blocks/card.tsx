22be5d1b064796f9c80fb210d676a2465fdca73a
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2025-03-11 11:13:17 +0100

    initial push

--- src/blocks/card.tsx
+++ src/blocks/card.tsx
@@ -1,5 +1,11 @@
-export const Card = () => {
+export default function Card() {
     return (
-        <div>card</div>
+        <div className="w-[190px] h-[190px] px-1.5 py-3 bg-red-200 rounded-3xl inline-flex flex-col items-end justify-between">
+            <p className="w-full text-[13px] leading-[13px] text-white/80">om 1-3 dagar</p>
+            <article>
+                <h2 className="font-inter font-semibold text-[1rem] leading-19px]">ICA Bollebygd</h2>
+                <p></p>
+            </article>
+        </div>
     )
 };
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,7 +1,22 @@
+import Card from "../../blocks/card"
+
 export default function DashBoard() {
     return (
-        <main>
-            <h1>Login Page</h1>
+        <main className="min-h-[59.75rem] w-full bg-gradient-primary inline-flex flex-col items-center justify-start pt-[3.125rem]">
+            <div className="w-[25.5rem] inline-flex flex-col items-center justify-start gap-6">
+                <section>icons</section>
+                <h1>Välkommen, Michael!</h1>
+                <div className="w-full inline-flex flex-col items-end justify-center gap-3">
+                    <article>
+                        <p>Dina pågående beställningar</p>
+                        <button>Se alla</button>
+                    </article>
+                    <div className="inline-flex items-center justify-center gap-3 py-2">
+                        <Card />
+                    </div>
+                    <button>Skapa ny beställning</button>
+                </div>
+            </div>
         </main>
     )
 };
