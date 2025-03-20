15b8db263be07f3089e63b3fc4b453faba6fc106
CalleTrogen <calleat@hotmail.com>
2025-03-20 13:41:26 +0100

    added carousell

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,10 +2,38 @@ import { cn } from "../../lib/utils";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function ButtonPrimary({className, ...props}: ButtonProps) {
+export default function ButtonPrimary({ className, ...props }: ButtonProps) {
     return (
         <button className={cn("font-lato font-bold w-[232px] px-4 py-3 bg-Branding-primaryButton hover:bg-Branding-primaryButton/80 transition-colors rounded-xl border border-Branding-textAccent inline-flex justify-center items-center text-Branding-textAccent text-[1.125rem] leading-[1.5rem]", className)}
-        {...props}>
+            {...props}>
         </button>
     )
+};
+
+export const ButtonSecondary: React.FC<ButtonProps> = ({ className, ...props }) => {
+    return (
+        <button
+            className={cn(
+                "font-lato font-semibold w-auto px-4 py-3 bg-blue-800 hover:bg-blue-500 text-white border border-gray-300 rounded-[12px] transition-colors inline-flex justify-center items-center text-[1.125rem] leading-[1.5rem]",
+                className
+            )}
+            {...props}
+        >
+            Se alla
+        </button>
+    );
+};
+
+export const ButtonTertiary: React.FC<ButtonProps> = ({ className, ...props }) => {
+    return (
+        <button
+            className={cn(
+                "font-lato font-semibold w-auto px-4 py-3 bg-gray-700 hover:bg-gray-500 text-[rgb(232,185,35)] border border-[rgb(232,185,35)] rounded-[12px] transition-colors inline-flex justify-center items-center text-[1.125rem] leading-[1.5rem]",
+                className
+            )}
+            {...props}
+        >
+            Se alla
+        </button>
+    );
 };
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,5 +1,17 @@
-import { Card, CardDate, CardHeader, CardStore, CardAddress, CardFooter, CardClientName, CardClientNumber } from "../../blocks/card";
-import customersData from '../../../customers.json';
+import { useRef } from "react";
+import {
+    Card,
+    CardDate,
+    CardHeader,
+    CardStore,
+    CardAddress,
+    CardFooter,
+    CardClientName,
+    CardClientNumber,
+} from "../../blocks/card";
+import customersData from "../../../customers.json";
+import { ButtonSecondary, ButtonTertiary } from "./../../components/ui/button";
+import Menu from "./../../elements/menu/menu";
 
 type Address = {
     street: string;
@@ -20,82 +32,115 @@ export default function DashBoard() {
     const customers: Customer[] = customersData.customers;
     const previousOrders: Customer[] = customersData.previousOrders;
 
+    // Creates a ref for the carousel
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    const scrollLeft = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+        }
+    };
+
     return (
-        <main className="min-h-[59.75rem] w-full bg-gradient-primary inline-flex flex-col items-center justify-start pt-[3.125rem]">
+        <main className="min-h-[59.75rem] w-full bg-gradient-primary inline-flex flex-col items-center justify-start pt-[3.125rem] relative">
+            <Menu />
             <div className="w-[25.5rem] inline-flex flex-col items-center justify-start gap-6">
                 <section className="h-11">icons</section>
-                <h1 className="font-open-sans font-bold text-[2rem] leading-[2.75rem] text-Branding-textAccent">Välkommen, Michael!</h1>
+                <h1 className="font-open-sans font-bold text-[2rem] leading-[2.75rem] text-Branding-textAccent">
+                    Välkommen, Michael!
+                </h1>
 
                 {/* Current orders */}
-                <div className="w-full inline-flex flex-col items-end justify-center gap-3">
+                <div className="w-full inline-flex flex-col items-end justify-center gap-3 mt-5">
                     <div className="w-full inline-flex flex-col items-center justify-center gap-3">
                         <article className="w-[380px] flex items-center justify-between">
                             <p className="font-open-sans font-semibold text-[1.125rem] leading-[1.375rem] text-Branding-textPrimary">
                                 Dina pågående beställningar
                             </p>
-                            <button>Se alla</button>
+                            <ButtonSecondary>Se alla</ButtonSecondary>
                         </article>
-                        <div className="inline-flex items-center justify-center gap-3 py-2">
-                            {customers.map((customer) => (
-                                <Card key={customer.customerNumber}>
-                                    <CardDate>{customer.date}</CardDate>
-                                    <CardHeader>
-                                        <CardStore>{customer.name}</CardStore>
-                                        <CardAddress>
-                                            {customer.address.street} {customer.address.postalCode} {customer.address.city}
-                                        </CardAddress>
-                                    </CardHeader>
-                                    <CardFooter>
-                                        <CardClientName>{customer.contactPerson}</CardClientName>
-                                        <CardClientNumber>{customer.customerNumber}</CardClientNumber>
-                                    </CardFooter>
-                                </Card>
-                            ))}
+
+                        {/* Carousel Container */}
+                        <div className="relative w-full">
+                            {/* Button left */}
+                            <button
+                                onClick={scrollLeft}
+                                className="absolute left-0 top-1/2 transform -translate-y-1/2 p-0 w-8 h-8 bg-gray-600 rounded-full z-10"
+                            >
+                                ←
+                            </button>
+
+                            {/* Scrollbar-container */}
+                            <div
+                                ref={scrollRef}
+                                style={{
+                                    scrollbarWidth: "none",
+                                    msOverflowStyle: "none",
+                                }}
+                                className="w-full overflow-x-scroll scrollbar-hide inline-flex flex-row gap-5 snap-x snap-mandatory scroll-smooth"
+                            >
+                                {customers.map((customer) => (
+                                    <div key={customer.customerNumber} className="snap-center min-w-[300]">
+                                        <Card>
+                                            <CardDate>{customer.date}</CardDate>
+                                            <CardHeader>
+                                                <CardStore>{customer.name}</CardStore>
+                                                <CardAddress>
+                                                    {customer.address.street} {customer.address.postalCode}{" "}
+                                                    {customer.address.city}
+                                                </CardAddress>
+                                            </CardHeader>
+                                            <CardFooter>
+                                                <CardClientName>{customer.contactPerson}</CardClientName>
+                                                <CardClientNumber>{customer.customerNumber}</CardClientNumber>
+                                            </CardFooter>
+                                        </Card>
+                                    </div>
+                                ))}
+                            </div>
+
+                            {/* Button for right scroll */}
+                            <button
+                                onClick={scrollRight}
+                                className="absolute right-0 top-1/2 transform -translate-y-1/2 p-0 w-8 h-8 bg-gray-600 rounded-full z-10"
+                            >
+                                →
+                            </button>
                         </div>
                     </div>
-                    <button>Skapa ny beställning</button>
                 </div>
+            </div>
 
-                {/* Previous orders */}
-                <div className="w-full inline-flex flex-col items-center justify-center gap-3">
-                    <article className="w-[380px] flex items-center justify-between">
-                        <p className="font-open-sans font-semibold text-[1.125rem] leading-[1.375rem] text-Branding-textPrimary">
-                            Dina tidigare beställningar
-                        </p>
-                        <button>Se alla</button>
-                    </article>
-                    <div className="inline-flex items-center justify-center gap-3 py-2">
-                        {previousOrders.map((order) => (
-                            <Card key={order.customerNumber}>
-                                <CardDate>{order.date}</CardDate>
-                                <CardHeader>
-                                    <CardStore>{order.name}</CardStore>
-                                    <CardAddress>
-                                        {order.address.street} {order.address.postalCode} {order.address.city}
-                                    </CardAddress>
-                                </CardHeader>
-                                <CardFooter>
-                                    <CardClientName>{order.contactPerson}</CardClientName>
-                                    <CardClientNumber>{order.customerNumber}</CardClientNumber>
-                                </CardFooter>
-                            </Card>
-                        ))}
-                        {previousOrders.map((order) => (
-                            <Card key={order.customerNumber + "2"}>
-                                <CardDate>{order.date}</CardDate>
-                                <CardHeader>
-                                    <CardStore>{order.name}</CardStore>
-                                    <CardAddress>
-                                        {order.address.street} {order.address.postalCode} {order.address.city}
-                                    </CardAddress>
-                                </CardHeader>
-                                <CardFooter>
-                                    <CardClientName>{order.contactPerson}</CardClientName>
-                                    <CardClientNumber>{order.customerNumber}</CardClientNumber>
-                                </CardFooter>
-                            </Card>
-                        ))}
-                    </div>
+            {/* Previous orders */}
+            <div className="w-full inline-flex flex-col items-center justify-center gap-3 mt-5">
+                <article className="w-[380px] flex items-center justify-between">
+                    <p className="font-open-sans font-semibold text-[1.125rem] leading-[1.375rem] text-Branding-textPrimary">
+                        Dina tidigare beställningar
+                    </p>
+                    <ButtonTertiary>Se alla</ButtonTertiary>
+                </article>
+                <div className="inline-flex items-center justify-center gap-3 py-2">
+                    {previousOrders.map((order) => (
+                        <Card key={order.customerNumber}>
+                            <CardDate>{order.date}</CardDate>
+                            <CardHeader>
+                                <CardStore>{order.name}</CardStore>
+                                <CardAddress>
+                                    {order.address.street} {order.address.postalCode} {order.address.city}
+                                </CardAddress>
+                            </CardHeader>
+                            <CardFooter>
+                                <CardClientName>{order.contactPerson}</CardClientName>
+                                <CardClientNumber>{order.customerNumber}</CardClientNumber>
+                            </CardFooter>
+                        </Card>
+                    ))}
                 </div>
             </div>
         </main>
