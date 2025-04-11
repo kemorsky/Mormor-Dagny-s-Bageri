64e27b32321521f4333e387ece20774548cfa630
CalleTrogen <calleat@hotmail.com>
2025-04-11 13:22:15 +0200

    fetch data to dashboard

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,47 +1,47 @@
 import { OrderDetails, Order } from "../types/types";
 
 type RequestOptions = {
-    method?: string;
-    headers?: { [key: string]: string };
-    body?: string;
-  }
+  method?: string;
+  headers?: { [key: string]: string };
+  body?: string;
+}
 
 const BASE_URL = "http://localhost:5139/api";
 
 export const apiRequest = async (url: string, options: RequestOptions = {}) => {
   console.log(`Making request to ${url} with options:`, options);
 
-    try {
-      const token = sessionStorage.getItem("token");
-      if (!token) {
-        throw new Error("No token found");
-      }
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        ...options,
-      });
-      if (!response.ok) {
-        throw new Error(`API request failed: ${response.status}`);
-      }
-      return await response.json();
-      
-    } catch (error) {
-      console.error("API Error:", error);
-      throw error;
+  try {
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+      throw new Error("No token found");
     }
-  };
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      ...options,
+    });
+    if (!response.ok) {
+      throw new Error(`API request failed: ${response.status}`);
+    }
+    return await response.json();
+
+  } catch (error) {
+    console.error("API Error:", error);
+    throw error;
+  }
+};
 
 export const fetchStores = async () => {
-    try {
-      return await apiRequest(`${BASE_URL}/butiker`);
-    } catch (error) {
-      console.error("Error fetching stores:", error);
-      throw error;
-    }
+  try {
+    return await apiRequest(`${BASE_URL}/butiker`);
+  } catch (error) {
+    console.error("Error fetching stores:", error);
+    throw error;
+  }
 };
 
 export const fetchProducts = async () => {
@@ -58,10 +58,19 @@ export const pushOrder = async (orderDetails: OrderDetails) => {
     const response = await apiRequest(`${BASE_URL}/beställningsdetaljer`, {
       method: 'POST',
       body: JSON.stringify(orderDetails),
-  });
-      return await response.json();   
+    });
+    return await response.json();
   } catch (error) {
     console.error("Error creating order:", error);
+    throw error;
+  }
+};
+
+export const fetchOrders = async () => {
+  try {
+    return await apiRequest(`${BASE_URL}/beställningar`);
+  } catch (error) {
+    console.error("Error fetching orders:", error);
     throw error;
   }
 };
